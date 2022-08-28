04dd20d5892b342b44c60c13265396ed23479c5a
Rodrigo Adalton <rodrigoadalton@hotmail.com>
2022-08-28 17:11:46 -0300

    new project MessageBus

--- GeekShopping.CartAPI/Controllers/CartController.cs
+++ GeekShopping.CartAPI/Controllers/CartController.cs
@@ -1,5 +1,6 @@
 using GeekShopping.CartAPI.Data.ValueObjects;
 using GeekShopping.CartAPI.Messages;
+using GeekShopping.CartAPI.RabbitMQSender;
 using GeekShopping.CartAPI.Repository;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,10 +11,12 @@ namespace GeekShopping.CartAPI.Controllers
     public class CartController : ControllerBase
     {
         private ICartRepository _repository;
+        private IRabbitMQMessageSender _rabbitMQMessageSender;
 
-        public CartController(ICartRepository repository)
+        public CartController(ICartRepository repository, IRabbitMQMessageSender rabbitMQMessageSender)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _rabbitMQMessageSender = rabbitMQMessageSender ?? throw new ArgumentNullException(nameof(rabbitMQMessageSender));
         }
 
         [HttpGet("find-cart/{id}")]
@@ -73,12 +76,17 @@ namespace GeekShopping.CartAPI.Controllers
         [HttpPost("checkout")]
         public async Task<ActionResult<CheckoutHeaderVO>> Checkout(CheckoutHeaderVO vo)
         {
+            if (vo?.UserId == null) return BadRequest();
+
             var cart = await _repository.FindCartByUserId(vo.UserId);
+
             if (cart == null) return NotFound();
+
             vo.CartDetails = cart.CartDetails;
             vo.DateTime = DateTime.Now;
 
-            //TASK RabbitMQ logic comes here!!!
+            // RabbitMQ logic comes here!!!
+            _rabbitMQMessageSender.SendMessage(vo, "checkoutqueue");
 
             return Ok(vo);
         }
---
+++ GeekShopping.MessageBus/BaseMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekShopping.MessageBus
+{
+    public class BaseMessage
+    {
+        public long Id { get; set; }
+        public DateTime MessageCreated { get; set; }
+    }
+}
+
+
+/*
+ * https://hub.docker.com/_/rabbitmq
+
+docker run -d --hostname my-rabbit --name some-rabbit -p 15672:15672 -p 5672:5672 rabbitmq:3-management
+
+localhost: 15672
+
+User: guest
+Password: guest
+*/
